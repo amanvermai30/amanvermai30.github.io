a51ea23af90025023d5d353861b0fedd77dd2627
Aman Verma <amancreative9598@gmail.com>
2022-10-18 21:08:51 +0530

    day-5 work

--- script/index.js
+++ script/index.js
@@ -2,6 +2,29 @@ const appPortfolioList = document.querySelectorAll('.app__portfolio--list');
 const appPortfolioView = document.querySelectorAll('app__portfolio--view');
 
 
+// Reveale section
+// select all section
+const allSection = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target)
+}
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.20
+})
+allSection.forEach((section) => {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+})
+
+
+
+// revealing view&github link
 appPortfolioList.forEach((e) => {
     e.addEventListener('mouseover', (e) => {
         e.preventDefault();
@@ -17,3 +40,5 @@ appPortfolioList.forEach((e) => {
         }
     })
 })
+
+
