db388e023ee15001da71b4a8bcb47894a25d448b
Aman Verma <amancreative9598@gmail.com>
2022-10-19 19:48:53 +0530

    day-6 work

--- script/index.js
+++ script/index.js
@@ -2,6 +2,58 @@ const appPortfolioList = document.querySelectorAll('.app__portfolio--list');
 const appPortfolioView = document.querySelectorAll('app__portfolio--view');
 
 
+const navliAbout = document.querySelector(".liabout")
+const liSkill = document.querySelector(".liskill")
+const liWork = document.querySelector(".liwork")
+const liContact = document.querySelector(".licontact")
+
+
+const sectionAbout = document.querySelector(".section__about")
+const sectionSkill = document.querySelector(".about--skillex")
+const sectionWork = document.querySelector(".section__portfolio")
+const sectionContact = document.querySelector(".section__contact")
+
+
+navliAbout.addEventListener('click', (e) => {
+    // scrolling
+    sectionAbout.scrollIntoView({ behavior: 'smooth' });
+})
+
+liSkill.addEventListener('click', (e) => {
+    // scrolling
+    sectionSkill.scrollIntoView({ behavior: 'smooth' });
+})
+
+liWork.addEventListener('click', (e) => {
+    // scrolling
+    sectionWork.scrollIntoView({ behavior: 'smooth' });
+})
+
+liContact.addEventListener('click', (e) => {
+    // scrolling
+    sectionContact.scrollIntoView({ behavior: 'smooth' });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Reveale section
 // select all section
 const allSection = document.querySelectorAll('.section');
@@ -15,7 +67,7 @@ const revealSection = function (entries, observer) {
 }
 const sectionObserver = new IntersectionObserver(revealSection, {
     root: null,
-    threshold: 0.20
+    threshold: 0.07
 })
 allSection.forEach((section) => {
     sectionObserver.observe(section);
